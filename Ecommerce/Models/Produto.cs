7a03a94001b3dca6d42e7a16067e62b3aa28f54a
jean <jcl.ribeiro@hotmail.com>
2019-10-10 20:18:37 -0300

    Criação do repositorio

---
+++ Ecommerce/Models/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+        public String Nome { get; set; }
+        public String Descricao { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public Produto()
+        {
+            CriadoEm = DateTime.Now;
+        }
+
+
+    }
+}
